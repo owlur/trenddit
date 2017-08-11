d48da0dbe5399f52dc8fb6aedf1e1a5879ba4dc3
owlur <8864248@naver.com>
2017-08-11 22:33:00 +0900

    class 제거

--- module/analyse.py
+++ module/analyse.py
@@ -2,268 +2,278 @@ import nltk
 from collections import defaultdict
 
 from module import access_db
+from module import date as dt
 
 
-class Analyse:
-    def posts_analyze(self, posts):
-        if type(posts) is list:
-            documents = []
+def posts_analyze(posts):
+    if type(posts) is list:
+        documents = []
 
-            for post in posts:
-                documents.append(self.posts_analyze(post))
+        for post in posts:
+            documents.append(posts_analyze(post))
 
-            return documents
+        return documents
 
-        elif type(posts) is dict:
-            document = {'ID': posts['id']}
+    elif type(posts) is dict:
+        document = {'ID': posts['id']}
 
-            document.update(self.parse_text(posts['title']))
+        document.update(parse_text(posts['title']))
 
-            if posts['content']:
-                document.update(self.parse_text(posts['content']))
+        if posts['content']:
+            document.update(parse_text(posts['content']))
 
-            if posts['comments']:
-                document['COMMENTS'] = []
+        if posts['comments']:
+            document['COMMENTS'] = []
 
-                for comment in posts['comments']:
-                    comment_noun = self.parse_text(comment['content'])
-                    comment_noun['ID'] = comment['id']
+            for comment in posts['comments']:
+                comment_noun = parse_text(comment['content'])
+                comment_noun['ID'] = comment['id']
 
-                    document['COMMENTS'].append(comment_noun)
+                document['COMMENTS'].append(comment_noun)
 
-            return document
+        return document
 
-    def parse_text(self, text):
-        return self.count_noun(self.tokenize(text))
 
-    def tokenize(self, text):
-        tokens = nltk.word_tokenize(text)
-        tagged_tokens = nltk.pos_tag(tokens)
+def parse_text(text):
+    return count_noun(tokenize(text))
 
-        # parser_en = nltk.RegexpParser("NP: {<DT>?<JJ>?<NN.*>*}")
-        # chunks_en = parser_en.parse(tagged_tokens)
-        # chunks_en.draw()
 
-        return tagged_tokens
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    tagged_tokens = nltk.pos_tag(tokens)
 
-    def count_noun(self, tagged_tokens):
-        noun_dict = {}
-        except_noun = [".", "[", "]", ">", "<", "/*", "*/", "*", "+", "-", "=", "%"]
+    # parser_en = nltk.RegexpParser("NP: {<DT>?<JJ>?<NN.*>*}")
+    # chunks_en = parser_en.parse(tagged_tokens)
+    # chunks_en.draw()
 
-        for tagged_token in tagged_tokens:
-            if "NN" in tagged_token[1]:
+    return tagged_tokens
 
-                noun = tagged_token[0].lower()
 
-                if noun in except_noun or "." in noun:
-                    continue
+def count_noun(tagged_tokens):
+    noun_dict = {}
+    except_noun = [".", "[", "]", ">", "<", "/*", "*/", "*", "+", "-", "=", "%"]
 
-                if noun in noun_dict:
-                    noun_dict[noun] += 1
+    for tagged_token in tagged_tokens:
+        if "NN" in tagged_token[1]:
 
-                else:
-                    noun_dict[noun] = 1
+            noun = tagged_token[0].lower()
 
-        return noun_dict
+            if noun in except_noun or "." in noun:
+                continue
+
+            if noun in noun_dict:
+                noun_dict[noun] += 1
+
+            else:
+                noun_dict[noun] = 1
+
+    return noun_dict
 
 
 if __name__ == '__main__':
     a = access_db.AccessDB('reddit')
     b = a.find(collection='hacking')
 
-    ana = Analyse()
-    result = ana.posts_analyze(b)
+    result = posts_analyze(b)
+
+    print(result)
 
 
-class Score:
-    def make_id_list(date, end_date=None):
-        '''
-        하루 이상의 데이터를 이용할 경우 끝나는 날을 end_date 입력
-        make_id_list("20170101", end_date = "20170108")
-        Reddit.subreddits 에 있는 모든 서브레딧에서 부터 해당하는 날짜의 명사들을 가져옴
+def make_id_list(date, end_date=None):
+    '''
+    하루 이상의 데이터를 이용할 경우 끝나는 날을 end_date 입력
+    make_id_list("20170101", end_date = "20170108")
+    Reddit.subreddits 에 있는 모든 서브레딧에서 부터 해당하는 날짜의 명사들을 가져옴
 
-        '''
-        day = 86400
-        start_time = str2stamp(date)
+    '''
+    interval = 86400
+    start_time = dt.str2stamp(date)
 
-        if end_date:
-            end_time = str2stamp(end_date) + day
-        else:
-            end_time = start_time + day
+    if end_date:
+        end_time = dt.str2stamp(end_date) + interval
+    else:
+        end_time = start_time + interval
 
-        query = {"$and": [
-            {"date": {"$gte": start_time}},
-            {"date": {"$lt": end_time}}
-        ]}
+    query = {"$and": [
+        {"date": {"$gte": start_time}},
+        {"date": {"$lt": end_time}}
+    ]}
 
-        projection = {"_id": 0, "ID": 1}
+    projection = {"_id": 0, "ID": 1}
 
-        reddit = AccessDB('reddit')
+    reddit = AccessDB('reddit')
 
-        noun = AccessDB('noun')
+    noun = AccessDB('noun')
 
-        subreddits = Reddit().subreddits
+    subreddits = Reddit().subreddits
 
-        id_list = {}
+    id_list = {}
 
-        for subreddit in subreddits:
+    for subreddit in subreddits:
 
-            id_list[subreddit] = reddit.find(collection=subreddit, query=query, projection=projection)
+        id_list[subreddit] = reddit.find(collection=subreddit, query=query, projection=projection)
 
-            for ID in id_list[subreddit]:
-                id_query = {"ID": ID["ID"]}
-                noun_list = noun.find(collection=subreddit, query=id_query)
+        for ID in id_list[subreddit]:
+            id_query = {"ID": ID["ID"]}
+            noun_list = noun.find(collection=subreddit, query=id_query)
 
-                for nouns in noun_list:
-                    ID.update(nouns)
+            for nouns in noun_list:
+                ID.update(nouns)
 
-        return id_list
+    return id_list
 
-    def df(documents):
-        """
-        ID, _id, COMMENTS, 명사명단이 포함된
-        서브레딧하나만 받아야함
-        """
-        df = {}
 
-        exception = ["COMMENTS", "ID", "_id"]
+def df(documents):
+    """
+    ID, _id, COMMENTS, 명사명단이 포함된
+    서브레딧하나만 받아야함
+    """
+    df = {}
 
-        for submission in documents:
-            df[submission["ID"]] = set()
+    exception = ["COMMENTS", "ID", "_id"]
 
-            df[submission["ID"]] = set([i for i in submission if i not in exception])
+    for submission in documents:
+        df[submission["ID"]] = set()
 
-            if "COMMENTS" not in submission:
+        df[submission["ID"]] = set([i for i in submission if i not in exception])
+
+        if "COMMENTS" not in submission:
+            continue
+
+        for comment in submission["COMMENTS"]:
+            df[submission["ID"]] = df[submission["ID"]].union(set(
+                [i for i in comment if i not in exception]))
+
+    return df
+
+
+def tf(documents):
+    """
+    ID, _id, COMMENTS, 명사명단이 포함된
+    서브레딧하나만 받아야함
+    """
+    exception = ["COMMENTS", "ID", "_id"]
+
+    tf = {}
+    tf = defaultdict(lambda: 0, tf)
+
+    for submission in documents:
+        frequency = {}
+        frequency = defaultdict(lambda: 0, frequency)
+        total = 0
+
+        for noun_name in submission.keys():
+            if noun_name in exception:
                 continue
+            frequency[noun_name] += float(submission[noun_name])
+            total += submission[noun_name]
 
-            for comment in submission["COMMENTS"]:
-                df[submission["ID"]] = df[submission["ID"]].union(set(
-                    [i for i in comment if i not in exception]))
+        if "COMMENTS" not in submission:
+            continue
 
-        return df
-
-    def tf(documents):
-        """
-        ID, _id, COMMENTS, 명사명단이 포함된
-        서브레딧하나만 받아야함
-        """
-        exception = ["COMMENTS", "ID", "_id"]
-
-        tf = {}
-        tf = defaultdict(lambda: 0, tf)
-
-        for submission in documents:
-            frequency = {}
-            frequency = defaultdict(lambda: 0, frequency)
-            total = 0
-
-            for noun_name in submission.keys():
+        for comments_list in submission["COMMENTS"]:
+            for noun_name in comments_list.keys():
                 if noun_name in exception:
                     continue
-                frequency[noun_name] += float(submission[noun_name])
-                total += submission[noun_name]
 
-            if "COMMENTS" not in submission:
-                continue
+                frequency[noun_name] += float(comments_list[noun_name])
+                total += comments_list[noun_name]
 
-            for comments_list in submission["COMMENTS"]:
-                for noun_name in comments_list.keys():
-                    if noun_name in exception:
-                        continue
+        for noun_name in frequency.keys():
+            tf[noun_name] += frequency[noun_name] / total
 
-                    frequency[noun_name] += float(comments_list[noun_name])
-                    total += comments_list[noun_name]
+    # sorted_tf = sorted(tf.items(), key=itemgetter(1), reverse = True)
 
-            for noun_name in frequency.keys():
-                tf[noun_name] += frequency[noun_name] / total
+    return tf
 
-        # sorted_tf = sorted(tf.items(), key=itemgetter(1), reverse = True)
-
-        return tf
 
     # In[13]:
 
-    def score(for_tf, for_df):
-        all_df = {}
-        for subreddit in for_tf.keys():
-            all_df.update(df(for_tf[subreddit]))
 
-        for subreddit in for_df.keys():
-            all_df.update(df(for_df[subreddit]))
+def score(for_tf, for_df):
+    all_df = {}
+    for subreddit in for_tf.keys():
+        all_df.update(df(for_tf[subreddit]))
 
-        noun_df = {}
-        noun_df = defaultdict(lambda: 0, noun_df)
-        df_total = 0
+    for subreddit in for_df.keys():
+        all_df.update(df(for_df[subreddit]))
 
-        for sub_id in all_df.keys():
-            for keyword in all_df[sub_id]:
-                noun_df[keyword] += 1
-            df_total += 1
+    noun_df = {}
+    noun_df = defaultdict(lambda: 0, noun_df)
+    df_total = 0
 
-        all_tf = {}
+    for sub_id in all_df.keys():
+        for keyword in all_df[sub_id]:
+            noun_df[keyword] += 1
+        df_total += 1
 
-        for i in for_tf.keys():
-            all_tf[i] = tf(for_tf[i])
+    all_tf = {}
 
-        tf_idf = {}
-        for subreddit in all_tf.keys():
-            tf_idf[subreddit] = {}
-            for keyword in all_tf[subreddit]:
-                tf_idf[subreddit][keyword] = all_tf[subreddit][keyword] * math.log10(float(df_total) / noun_df[keyword])
+    for i in for_tf.keys():
+        all_tf[i] = tf(for_tf[i])
 
-        # sorted_tf_idf = {}
+    tf_idf = {}
+    for subreddit in all_tf.keys():
+        tf_idf[subreddit] = {}
+        for keyword in all_tf[subreddit]:
+            tf_idf[subreddit][keyword] = all_tf[subreddit][keyword] * math.log10(float(df_total) / noun_df[keyword])
 
-        # sorted_tf_idf['programming'] = sorted(tf_idf['programming'].items(), key=itemgetter(1), reverse = True)
+    # sorted_tf_idf = {}
 
-        return tf_idf
+    # sorted_tf_idf['programming'] = sorted(tf_idf['programming'].items(), key=itemgetter(1), reverse = True)
 
-    def test_insert_tf_idf(start_date, end_date):
-        scoreDB = AccessDB('score')
+    return tf_idf
 
-        stamp_start_date = str2stamp(start_date)
-        stamp_end_date = str2stamp(end_date)
 
-        date = stamp_start_date
+def test_insert_tf_idf(start_date, end_date):
+    scoreDB = AccessDB('score')
 
-        day = 86400
+    stamp_start_date = dt.str2stamp(start_date)
+    stamp_end_date = dt.str2stamp(end_date)
 
-        while (date <= stamp_end_date):
-            x_week_date = stamp2str(date - day * 7)
-            yesterday = stamp2str(date - day)
+    date = stamp_start_date
 
-            today = stamp2str(date)
+    day = 86400
 
-            today_list = make_id_list(today)
-            x_week_list = make_id_list(x_week_date, end_date=yesterday)
+    while (date <= stamp_end_date):
+        x_week_date = dt.stamp2str(date - day * 7)
+        yesterday = dt.stamp2str(date - day)
 
-            tf_idf = score(today_list, x_week_list)
+        today = dt.stamp2str(date)
 
-            scoreDB.insert("d" + today, tf_idf)
+        today_list = make_id_list(today)
+        x_week_list = make_id_list(x_week_date, end_date=yesterday)
 
-            date += day
+        tf_idf = score(today_list, x_week_list)
+
+        scoreDB.insert("d" + today, tf_idf)
+
+        date += day
+
 
     # In[107]:
 
-    def test_trend_score(date):
-        scoreDB = AccessDB('score')
 
-        documents = scoreDB.find(collection="d" + date)[0]
+def test_trend_score(date):
+    scoreDB = AccessDB('score')
 
-        x_week = date2list(date, date, pre_day=7)
-        x_week.remove(date)
+    documents = scoreDB.find(collection="d" + date)[0]
 
-        x_week_document = [scoreDB.find(collection="d" + i)[0] for i in x_week]
+    x_week = dt.date2list(date, date, pre_day=7)
+    x_week.remove(date)
 
-        keyword_score = {}
+    x_week_document = [scoreDB.find(collection="d" + i)[0] for i in x_week]
 
-        for subreddit in documents.keys():
-            keyword_score[subreddit] = {}
-            for keyword in documents[subreddit].keys():
-                score_sum = 0
-                for i in range(len(x_week_document)):
-                    if x_week_document[i][subreddit].get(keyword):
-                        score_sum += x_week_document[i][subreddit][keyword]
+    keyword_score = {}
 
-                keyword_score[subreddit][keyword] = documents[subreddit][keyword] - score_sum / len(x_week_document)
+    for subreddit in documents.keys():
+        keyword_score[subreddit] = {}
+        for keyword in documents[subreddit].keys():
+            score_sum = 0
+            for i in range(len(x_week_document)):
+                if x_week_document[i][subreddit].get(keyword):
+                    score_sum += x_week_document[i][subreddit][keyword]
 
-        return keyword_score
+            keyword_score[subreddit][keyword] = documents[subreddit][keyword] - score_sum / len(x_week_document)
+
+    return keyword_score
