7e2a40861653aa528c6411f07ff0532379559e3c
owlur <8864248@naver.com>
2017-09-07 23:02:08 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='trenddittools',
+    version='0.1',
+    packages=['module'],
+    url='https://github.com/owlur',
+    license='MIT',
+    author='owlur',
+    author_email='8864248@naver.com',
+    description='trenddit tools'
+)
